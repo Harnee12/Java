d06234fac7957eca293336d2e1caac39ff64992b
Harnee12 <113036399+Harnee12@users.noreply.github.com>
2022-09-15 15:26:19 +0530

    Add files via upload

---
+++ Lab/For2.java
@@ -0,0 +1,13 @@
+//HARNEE A
+//CG2576
+//3)b) Display the following sequence
+// 1 2 4 8
+class For2
+{ public static void main(String[] args)
+{
+for(int i=1;i<=10;i*=2)//using for loop
+{
+ System.out.println(i);
+}
+}
+}
