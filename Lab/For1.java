445747e843d0c9370c3ae0ed891b3e7c8bc5375a
Harnee12 <113036399+Harnee12@users.noreply.github.com>
2022-09-15 15:25:46 +0530

    Add files via upload

---
+++ Lab/For1.java
@@ -0,0 +1,13 @@
+//HARNEE A
+//CG2576
+//3)a) Display the following sequence
+// 1 4 7 10
+class For1
+{ public static void main(String[] args)
+{
+for(int i=1;i<=10;i+=3)//using for loop
+{
+ System.out.println(i);
+}
+}
+}
