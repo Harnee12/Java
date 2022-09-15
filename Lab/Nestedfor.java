d9f9160e0b4baffd26788381deefc566dd0f8ffc
Harnee12 <113036399+Harnee12@users.noreply.github.com>
2022-09-15 15:27:32 +0530

    Add files via upload

---
+++ Lab/Nestedfor.java
@@ -0,0 +1,15 @@
+//HARNEE A
+//CG2576
+//4)Print the pattern
+class Nestedfor
+{public static void main(String[] args)
+{for(int i=4;i>=0;i--)//rows
+{
+for(int j=0;j<=i;j++)//columns
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
