61e9508b4921f4b7145b72aef6350c98f0b8981f
Harnee12 <113036399+Harnee12@users.noreply.github.com>
2022-10-03 20:54:35 +0530

    Add files via upload

---
+++ Operator programs/Numbers.java
@@ -0,0 +1,15 @@
+//write a program to print number conversion
+class Numbers
+{
+	public static void main(String[] args)
+	{
+		int x=20;
+		int y=020;
+		int z=0b110;
+		int m=0x20;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	System.out.println(m);
+	}
+}
