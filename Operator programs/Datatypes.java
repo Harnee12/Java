4ddc6a9ee30401d793337408389f2b0f3755e213
Harnee12 <113036399+Harnee12@users.noreply.github.com>
2022-10-03 20:53:50 +0530

    Add files via upload

---
+++ Operator programs/Datatypes.java
@@ -0,0 +1,12 @@
+//Write a program to print integer datatypes
+class Datatypes
+{
+	public static void main(String[] args)
+	{
+		byte a=20;
+		short b=255;
+		int c=45000;
+		long d=100000;
+		System.out.println(a+" "+b+" "+c+" "+d);
+	}
+}
