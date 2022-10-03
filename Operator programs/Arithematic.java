4c2936728819abd0fd76c7eddaa70ffa64600de8
Harnee12 <113036399+Harnee12@users.noreply.github.com>
2022-10-03 20:52:59 +0530

    Add files via upload

---
+++ Operator programs/Arithematic.java
@@ -0,0 +1,22 @@
+//Write a program using Arithematic operators
+import java.util.Scanner;
+class Arithematic
+{
+	public static void main(String[] args)
+	{
+		int a,b;
+		System.out.println("Enter the elements a,b");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println("Arithematic operators ");
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
+
+		
+	
