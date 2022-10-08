2dcdad36ea6137139351c18e3eecdeaa6746bb5b
Harnee12 <113036399+Harnee12@users.noreply.github.com>
2022-10-08 21:43:38 +0530

    Add files via upload

---
+++ Oops Programs/Finaldemo4.java
@@ -0,0 +1,17 @@
+//final with static[cant change the value]
+class Simple
+{
+    static final int x=10;
+	void show()
+	{
+		System.out.println(x);
+	}
+}
+public class  Finaldemo4
+{
+	public static void main(String[] args)
+	{
+		Simple s=new Simple();
+		System.out.println(Simple.x));
+	}
+}
